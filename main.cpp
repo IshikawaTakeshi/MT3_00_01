79b32e2603ffe3e21eddecdd20b8b16bc139f00e
bluecrown5@ymobile.ne.jp <bluecrown2525@gmail.com>
2024-03-15 19:16:13 +0900

    Vector3の計算に関する関数の実装

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <Novice.h>
+#include "MyMath.h"
 
-const char kWindowTitle[] = "AL2_02_イシカワタケシ";
+static const int kRowHeight = 20;
+
+const char kWindowTitle[] = "イシカワタケシ_MT3_00_01";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -17,6 +20,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		// フレームの開始
 		Novice::BeginFrame();
 
+		Vector3 v1 = { 1.0f,3.0f,-5.0f };
+		Vector3 v2 = { 4.0f,-1.0f,2.0f };
+		float k = 4.0f;
+
+		Vector3 resultAdd = Add(v1, v2);
+		Vector3 resultSubtract = Subtract(v1, v2);
+		Vector3 resultMultiply = Multiply(k, v1);
+		float resultDot = Dot(v1, v2);
+		float resultLength = Length(v1);
+		Vector3 resultNormalize = Normalize(v2);
+
 		// キー入力を受け取る
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
@@ -32,6 +46,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		VectorScreenPrintf(0, 0, resultAdd, " : Add");
+		VectorScreenPrintf(0, kRowHeight, resultSubtract, " : Subtract");
+		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, " : Multiply");
+		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f : Dot", resultDot);
+		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f : Length", resultLength);
+		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, " : Normalize");
 
 		///
 		/// ↑描画処理ここまで
